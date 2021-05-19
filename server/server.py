1616ba84c4b2223bc8dad13af4c49f7cb4df26db
Mahog <6837928+Mahog@users.noreply.github.com>
2021-05-19 09:24:21 +0200

    fix case sample size is larger than remaining data

--- server/server.py
+++ server/server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 
 RANDOM_DATASET_SIZE = 1000000
-RANDOM_DATASET_DIMENSIONS = 10
+RANDOM_DATASET_DIMENSIONS = 2
 RANDOM_SAMPLE = uniform(0, 1, RANDOM_DATASET_SIZE * RANDOM_DATASET_DIMENSIONS)
 RANDOM_DATASET = RANDOM_SAMPLE.reshape((RANDOM_DATASET_SIZE, RANDOM_DATASET_DIMENSIONS))
 
@@ -23,28 +23,39 @@ sampling_b = np.zeros(RANDOM_DATASET_SIZE)
 def hello_world():
   return 'Ok. Flask server successfully launched.'
 
-@app.route('/sample', methods=["GET"])
-def sample():
-  sample_size = int(request.args.get("size"))
-  sampling = sampling_a if request.args.get("sample") == "a" else sampling_b
 
-  remaining_points = []
-  for i in indexes:
-    if sampling[i] == 0:
-      remaining_points.append(i)
-
-  sampled_indeces = random.sample(remaining_points, sample_size)
-  sampled_data = [RANDOM_DATASET[i].tolist() for i in sampled_indeces]
-  sampling[np.array(sampled_indeces)] = 1
-
-  timestamp = datetime.now()
+def produce_response_for_sample(sample_as_list):
   payload = {
-    "timestamp": str(timestamp),
-    "sample": sampled_data
+    "timestamp": str(datetime.now()),
+    "sample": sample_as_list
   }
 
   response = jsonify(payload)
   response.headers.add("Access-Control-Allow-Origin", "*")
+
+  return response
+
+
+@app.route('/sample', methods=["GET"])
+def sample():
+  sample_size = int(request.args.get("size"))
+  sampling = sampling_a if request.args.get("sample") == "a" else sampling_b
+  sampled = sampling.sum()
+
+  remaining_indeces = sampling == 0
+  remaining = remaining_indeces.sum()
+
+  if remaining == 0:
+    return produce_response_for_sample([])
+
+  sample_size = sample_size if sampled+sample_size < RANDOM_DATASET_SIZE else RANDOM_DATASET_SIZE-sampled
+  sample_size = int(sample_size)
+
+  sampled_indeces = random.sample(list(remaining_indeces), sample_size)
+  sampled_data = [RANDOM_DATASET[i].tolist() for i in sampled_indeces]
+  sampling[np.array(sampled_indeces)] = 1
+
+  response = produce_response_for_sample(sampled_data)
 
   return response
 
