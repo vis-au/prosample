04ac81a47509e70bca9f512c20f8bdb49bc1ee8c
Mahog <6837928+Mahog@users.noreply.github.com>
2021-05-20 11:23:53 +0200

    normalize sampled item positions to [0,1]

--- server/server.py
+++ server/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, abort, jsonify, request
+import numpy as np
 from datetime import datetime
 from pipeline import Pipeline
 
@@ -14,7 +15,7 @@ def hello_world():
   return 'Ok. Flask server successfully launched.'
 
 
-def create_response(payload):
+def cors_response(payload):
   response = jsonify(payload)
   response.headers.add("Access-Control-Allow-Origin", "*")
   return response
@@ -26,33 +27,40 @@ def produce_response_for_sample(sample_as_list):
     "sample": sample_as_list
   }
 
-  return create_response(payload)
+  return cors_response(payload)
 
 
-@app.route('/create_pipeline/<id>', methods=["PUT"])
+def get_pipeline_config(req):
+  configuration = {
+    "linearization": req.args.get("linearization"),
+    "subdivision": req.args.get("subdivision"),
+    "selection": req.args.get("selection"),
+    "data": req.args.get("data"),
+  }
+
+  return configuration
+
+
+@app.route('/create_pipeline/<id>', methods=["GET"])
 def create_pipeline(id):
-  configuration = request.args.get("configuration")
-  pipeline = Pipeline(configuration)
-
-  PIPELINES[str(id)] = pipeline
-  return "ok"
+  PIPELINES[str(id)] = Pipeline(get_pipeline_config(request))
+  return cors_response("ok")
 
 
-@app.route('/update_pipeline/<id>', methods=["PUT"])
+@app.route('/update_pipeline/<id>', methods=["GET"])
 def update_pipeline(id):
   old_pipeline = PIPELINES.get(id)
+  config = get_pipeline_config(request)
 
   if old_pipeline == None:
-      create_pipeline(id)
+    PIPELINES[str(id)] = Pipeline(config)
+    return cors_response("ok")
 
-  configuration = request.args.get("configuration")
-  pipeline = Pipeline(configuration)
-
-  PIPELINES[id] = pipeline
-  return "ok"
+  old_pipeline.update_selection(config["selection"])
+  return cors_response("ok")
 
 
-@app.route('/set_selection/<id>', methods=["PUT"])
+@app.route('/set_selection/<id>', methods=["GET"])
 def set_selection(id):
   pipeline = PIPELINES.get(id)
 
@@ -62,8 +70,25 @@ def set_selection(id):
 
   selection = request.args.get("selection")
   pipeline.update_selection(selection)
-  return "ok"
+  return cors_response("ok")
 
+
+def normalize_chunk_positions(chunk):
+  x = chunk[:, 1]
+  y = chunk[:, 2]
+
+  min_x = x[x[0] != None].min()
+  min_y = y[y[0] != None].min()
+  max_x = x[x[0] != None].max()
+  max_y = y[y[0] != None].max()
+
+  normalized_x = (x - min_x) / (max_x - min_x)
+  normalized_y = (y - min_y) / (max_y - min_y)
+
+  chunk[:, 1] = normalized_x
+  chunk[:, 2] = normalized_y
+
+  return chunk
 
 @app.route('/sample/<id>', methods=["GET"])
 def sample(id):
@@ -74,14 +99,15 @@ def sample(id):
       abort(400)
 
   next_chunk = pipeline.get_next_chunk()
-  return produce_response_for_sample(next_chunk.tolist())
+  normalized_chunk = normalize_chunk_positions(next_chunk)
+  return produce_response_for_sample(normalized_chunk.tolist())
 
 
 @app.route('/reset', methods=["GET"])
 def reset_pipelines():
   global PIPELINES
   PIPELINES = {}
-  return "ok"
+  return cors_response("ok")
 
 
 if __name__ == "__main__":
