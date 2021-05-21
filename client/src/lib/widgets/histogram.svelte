ab27303cdc28a207b7f3afa7a2111e2659239333
Mahog <6837928+Mahog@users.noreply.github.com>
2021-05-21 15:50:25 +0200

    add vega lite histogram component

---
+++ client/src/lib/widgets/histogram.svelte
@@ -0,0 +1,30 @@
+<script lang="typescript">
+  import VegaLitePlot from "./vega-lite-plot.svelte";
+
+  export let width = 100;
+  export let height = 100;
+  export let data: Array<any>;
+  export let dimension: string;
+
+  $: histogram = {
+    $schema: "https://vega.github.io/schema/vega-lite/v5.json",
+    data: {
+      values: data
+    },
+    width: width,
+    height: height,
+    margin: "none",
+    mark: "bar",
+    encoding: {
+      x: {
+        bin: true,
+        field: dimension
+      },
+      y: {
+        aggregate: "count"
+      }
+    }
+  }
+</script>
+
+<VegaLitePlot id="dimension-histogram" spec={ histogram } />
---
+++ client/src/lib/widgets/vega-lite-plot.svelte
@@ -0,0 +1,25 @@
+<script lang="typescript">
+  import { afterUpdate } from "svelte";
+
+  export let id: string;
+  export let spec: any;
+
+  let vegaEmbed;
+
+  afterUpdate(() => {
+    vegaEmbed = (window as any).vegaEmbed;
+    if (vegaEmbed === undefined) {
+      return;
+    }
+    vegaEmbed.embed(`#${id}-vega-container`, spec);
+  });
+
+</script>
+
+<div id="{id}-vega-container"></div>
+
+<svelte:head>
+	<script src="https://cdn.jsdelivr.net/npm/vega"></script>
+	<script src="https://cdn.jsdelivr.net/npm/vega-lite"></script>
+	<script src="https://cdn.jsdelivr.net/npm/vega-embed"></script>
+</svelte:head>
