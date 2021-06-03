38cd7c795dc2044a2c7bb12a7e5456fbbc644233
Mahog <6837928+Mahog@users.noreply.github.com>
2021-06-03 11:02:24 +0200

    deselect bins when zooming

--- client/src/lib/state/selected-bin.ts
+++ client/src/lib/state/selected-bin.ts
@@ -1,5 +1,8 @@
 import type { BinType } from "$lib/util/bin-generator";
 import type { HexbinBin } from "d3-hexbin";
 import { writable } from "svelte/store";
+import { currentTransform } from "./zoom-transform";
 
 export const selectedBins = writable([] as HexbinBin<BinType>[]);
+
+currentTransform.subscribe(() => selectedBins.set([]));
