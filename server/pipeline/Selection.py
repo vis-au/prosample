d72b7bbf727682fbe5ec6f63799ccaa6e8ef2512
Mahog <6837928+Mahog@users.noreply.github.com>
2022-07-07 16:32:49 +0200

    integrate proof-of-concept steering into pipeline

--- server/pipeline/Selection.py
+++ server/pipeline/Selection.py
@@ -1,14 +1,85 @@
 from abc import ABC, abstractmethod
-import numpy as np
+from typing import List
 import random
+import itertools
+import numpy as np
 
 
 class Selection(ABC):
+    def __init__(self) -> None:
+        super().__init__()
+        self.steer()
+
+    def steer(self, dimension: int=None, min_value: int=None, max_value: int=None):
+        self.steering = {
+            "dimension": 2,  # on spotify this is the "duration" dimension
+            "min_value": 3000,
+            "max_value": 40000
+        }
 
     def load_subdivision(self, subdivision):
         self.subdivision = subdivision
 
-    def next_chunk(self):
+    def is_steered_subspace_empty(self):
+        # FIXME: this is a sinful hack, compacting all items into a numpy array to make queries fast
+        all_items = np.array(list(itertools.chain.from_iterable(list(self.subdivision.values()))))
+
+        dim = self.steering["dimension"]
+        min_value = self.steering["min_value"]
+        max_value = self.steering["max_value"]
+        check = (all_items[:, dim] >= min_value) & (all_items[:, dim] <= max_value)
+        is_empty = check.sum() == 0
+
+        return is_empty
+
+    def get_indeces_matching_steering_in_bucket(self, bucket_index: int) -> List[int]:
+        dim = self.steering["dimension"]
+        bucket = np.array(self.subdivision[bucket_index])[:, dim]
+
+        min_value = self.steering["min_value"]
+        max_value = self.steering["max_value"]
+        matches_query = (bucket >= min_value) & (bucket <= max_value)
+
+        # use the 0 index below, because nonzero() gets indeces, but returns a tuple
+        indeces = matches_query.nonzero()[0].tolist()
+        return indeces
+
+    def create_steered_chunk(self, chunk_size: int) -> np.ndarray:
+        # proof-of-concept: steering means that we can prioritize data along ONE dimension in the
+        # data. Thus, we go through all subdivision and collect items that match the steering
+        # condition, until we have `chunk_size` items
+        steered_chunk = []
+        buckets = self.subdivision.copy()
+
+        matches_query_tuples = []
+
+        for b in buckets:
+            matching_indeces = self.get_indeces_matching_steering_in_bucket(b)
+
+            for matching_index in matching_indeces:
+                matches_query_tuples += [(b, matching_index)]
+
+        actual_chunk_size = min(chunk_size, len(matches_query_tuples))
+
+        # add the number of matching items, but at most chunk_size items to the steered chunk
+        for i in range(0, actual_chunk_size):
+            b, matching_index = matches_query_tuples[i]
+            steered_chunk += [buckets[b][matching_index]]
+            buckets[b][matching_index] = None  # set flag for deletion
+
+        for b in buckets:
+            self.subdivision[b] = [
+                item for item in buckets[b] if item is not None
+            ]
+
+            # delete the entire bucket if it's empty
+            if len(self.subdivision[b]) == 0:
+                del self.subdivision[b]
+
+        steered_chunk = [c for c in steered_chunk if c is not None]
+        return np.array(steered_chunk)
+
+    def next_chunk(self) -> np.ndarray:
         # If the first subdivision is empty, None is returned
         if len(self.subdivision) == 0:
             return None
@@ -16,6 +87,16 @@ class Selection(ABC):
         data_dimension = len(self.subdivision[first_key][0])
         chunk = np.full((len(self.subdivision), data_dimension), None)
         chunk_index = 0
+
+        # this is a simple extension for enabling steering: check if steering parameters are set,
+        # if that steering subspace is not empty and then sample from there, otherwise use the
+        # default strategy for selecting
+        if self.steering["dimension"] is not None:
+            if not self.is_steered_subspace_empty():
+                # use number of buckets as chunk size
+                chunk = self.create_steered_chunk(len(self.subdivision))
+                return chunk
+
         keys = self.subdivision.copy()
         for i in keys:
             next_index = self.select_element(chunk, chunk_index, i)
@@ -27,13 +108,14 @@ class Selection(ABC):
 
     # Selects from bucket bucket_number, expands chunk at index chunk_index with it and returns which index was selected
     @abstractmethod
-    def select_element(self, chunk, chunk_index, bucket_number):
+    def select_element(self, chunk, chunk_index, bucket_number) -> int:
         pass
 
 
 class SelectionRandom(Selection):
 
     def __init__(self, seed=0):
+        super().__init__()
         self.seed = seed
 
     def select_element(self, chunk, chunk_index, bucket_number):
@@ -56,6 +138,7 @@ class SelectionMinimum(Selection):
     attribute = 0
 
     def __init__(self, attribute):
+        super().__init__()
         self.attribute = attribute
 
     def select_element(self, chunk, chunk_index, bucket_number):
@@ -69,6 +152,7 @@ class SelectionMaximum(Selection):
     attribute = 0
 
     def __init__(self, attribute):
+        super().__init__()
         self.attribute = attribute
 
     def select_element(self, chunk, chunk_index, bucket_number):
@@ -82,6 +166,7 @@ class SelectionMedian(Selection):
     attribute = 0
 
     def __init__(self, attribute):
+        super().__init__()
         self.attribute = attribute
 
     def select_element(self, chunk, chunk_index, bucket_number):
